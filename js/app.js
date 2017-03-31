9538b09dd6dc8b0c0cca23711258fae4e3a93412
Aleksandra Bogdanska <alexandra.bogdanska@gmail.com>
2017-03-31 22:14:23 +0200

    poprawki

--- js/app.js
+++ js/app.js
@@ -118,6 +118,25 @@ $(function() {
         }
     });
 
+    //animation starts on scroll
+
+    // function animationStartsWhenSeen(elem) {
+    //   var $elem = $(elem);
+    //
+    //   var scrollElem = ((navigator.userAgent.toLowerCase().indexOf("webkit") != -1) ? 'body' : 'html');
+    //   var viewportTop = $(scrollElem).scrollTop();
+    //   var viewportBottom = viewportTop + $(window).height();
+    //
+    //   var elemTop = Math.round($elem.offset().top);
+    //   var elemBottom = elemTop + $elem.height();
+    //
+    //   return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+    // }
+    //
+    // function checkAnimation() {
+    //   var $elem = $("#data").find(".data span");
+    // }
+
     //progressBars animation
 
     var data = $("#data");
